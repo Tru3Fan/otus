b65ecf54b570ecb4c29b0523341a0063196c2ec0
Tru3Fan <dmitriy.tru3fan@gmail.com>
2025-12-03 12:17:09 +0300

    create model

---
+++ internal/model/task/task.go
@@ -0,0 +1,23 @@
+package task
+
+import (
+	"time"
+)
+
+type Status string
+
+const (
+	StatusPending   Status = "pending"
+	StatusCompleted Status = "completed"
+	statusFailed    Status = "failed"
+)
+
+type Task struct {
+	ID          int64
+	Title       string
+	Description string
+	AssigneeID  int64
+	Deadline    time.Time
+	status      Status
+	createdAt   time.Time
+}
